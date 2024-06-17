1bb4a13ad1458626e3fc619c2bf816ea45bbc840
jschloty <personal.jts@gmail.com>
2024-06-17 19:01:57 -0400

    added untracked ref plugin

---
+++ plugins/brads-boilerplate-block-plugin/build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'ed25016b1a85eefed85e');
---
+++ plugins/brads-boilerplate-block-plugin/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => 'a454c922bade615b6517');
